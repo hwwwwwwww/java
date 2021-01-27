a53882f3782c7b3bdd009e296979dbba07df6ad9
hichem <hichem@192.168.56.1>
2021-01-28 00:43:38 +0100

    td1

--- TD1_M2103v2/src/Main.java
+++ TD1_M2103v2/src/Main.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner entrée = new Scanner(System.in);
-		System.out.println("Entrer un code de 4 chiffres :");
+		System.out.println("Code de 4 chiffres à saisir (1 à la fois)");
 		final int TAILLE = 4;
 		int[]code = new int[TAILLE];
-		entrée.close();
-		int somme = code[0] + code[1]	+ code[2];
-		if ((code[3]%10) == (somme%7)) {
+		for (int i = 0 ; i <TAILLE; i++) {
+			System.out.println("Entrez un chiffre :");
+			code[i]=entrée.nextInt();			
+		}
+		int somme = code[0] + code[1]+ code[2];
+		if ((code[3]%10) == (somme%7)){
 			System.out.println("Code bon");
 		} else {
 			System.out.println("Code incorrect");
-
 	}
+		entrée.close();
 	}
 }
