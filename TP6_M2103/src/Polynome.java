897d7beb45fe52d7e132cc5f6d940afea2a3baee
hichem <hichem@DESKTOP-RIE8QRJ>
2021-02-17 17:03:33 +0100

    tp6

---
+++ TP6_M2103/src/Polynome.java
@@ -0,0 +1,57 @@
+
+/**
+ * @author hichem
+ *
+ */
+
+public class Polynome {
+	/**	
+	 * construit un polynome
+	 */
+	public Polynome() {
+			
+	}
+	/**
+	 * ajoute un monome à un polynome
+	 * @param m
+	 * 	     monome à ajouter
+	 */
+	public void setMonome(Monome m) {
+		
+	}
+	/**	
+	 * retourne le monome du polynome
+	 * @return monome
+	 * 
+	 * @param e
+	 * 		exposant du monome
+	 * 		  
+	 */
+	public Monome getMonome(int e) throws ArithmeticException  {
+		return null;
+	}
+	
+	/**
+	 * calcul la somme de deux polynomes
+	 * @param p
+	 * 		  polynome à additioner
+	 */
+	public void somme(Polynome p) {
+		
+	}
+
+	/**
+	 * calcul le produit d'un polynome et d'un monome
+	 * @param m 
+	 *        monome du polynome
+	 */
+	public void produit(Monome m) {
+		
+	}
+	/**
+	 * calcul la dérivée d'un polynome
+	 */
+	public void dérivée() {
+		
+	}
+}
