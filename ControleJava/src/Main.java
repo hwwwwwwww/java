fe2c8ec2befdb055ca18ed463e61df861c8a573b
hichem <snake9738@hotmail.com>
2021-02-28 14:20:12 +0100

    .

--- ControleJava/src/Main.java
+++ ControleJava/src/Main.java
@@ -7,21 +7,42 @@ public class Main {
 		System.out.println("Saisir le premier Intervalle");
 		int bInf = entrée.nextInt();
 		int bSup = entrée.nextInt();
-		Intervalle i = new Intervalle(bInf,bSup);
+		Intervalle i1 = new Intervalle(bInf,bSup);
 		System.out.println("Saisir le deuxième Intervalle");
 		int bInf2 = entrée.nextInt();
 		int bSup2 = entrée.nextInt();
 		Intervalle i2 = new Intervalle(bInf2,bSup2);
-		System.out.println(i);
-		System.out.println(i2);
 		entrée.close();
-		if(i.disjoint(i2)) {
+		if(i1.disjoint(i2)) {
 			System.out.println("Intervalles sont disjoints");
 		} else {
 			Intervalle i3 = new Intervalle(0,0);
-			i3 = i.inter(i2);
+			i3 = i1.inter(i2);
 			System.out.println("i3 = " + i3);
+			System.out.println("");
+			int TAILLE = i3.borneSup() - i3.borneInf() +1;
+			int [] t = new int[TAILLE] ;
+			int i = 0;
+			int image = i3.borneInf();
+			System.out.println("affichage première forme");
+			while(i<TAILLE) {	
+				t[i] = 10*image+ 5;
+				System.out.print(t[i]+"    ");
+				i++;
+				image++;
+			}
+			image = i3.borneInf();
+			System.out.println();
+			System.out.println();
+			System.out.println("affichage deuxième forme");
+			for(int n = 0 ; n<TAILLE;n++) {
+				t[n] = 10*image+ 5;
+				System.out.println("f("+image+")"+ " = " + t[n]);
+				image++;
+				
+			}
 			
+		
 		}
 		
 		
