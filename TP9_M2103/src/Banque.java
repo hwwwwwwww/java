4762e58d6ae60de2effd0931c46614e4d8af2dfe
krh3528a <krh3528a@r-info-rubi-e06.iut.iut-tlse3.fr>
2021-03-17 15:26:55 +0100

    .

--- TP9_M2103/src/Banque.java
+++ TP9_M2103/src/Banque.java
@@ -2,22 +2,23 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Banque extends CompteBancaire {
-	private List<CompteBancaire> comptes;
+	private List<CompteBancaire> l;
+	private String libelle;
 	public Banque(String numero) {
 		super(numero);
-		this.comptes= new LinkedList<>();
+		this.l= new LinkedList<CompteBancaire>();
 		
 	}
-	public String libelle() {
-		return this.libelle();
+	public String getLibelle() {
+		return this.libelle;
 	}
-	public void ouvrir(String numero, float valeur) throws ArithmeticException {
+	public void ouvrir(String numero, float v) throws ArithmeticException {
 		if(this.estCompteExistant(numero)) {
 			throw new ArithmeticException("compte déja existant");
 		}
 		CompteBancaire c = new CompteBancaire(numero);
-		c.déposer(valeur);
-		this.comptes.add(c);
+		c.déposer(v);
+		this.l.add(c);
 	} 
 	public void fermer(String numero) throws ArithmeticException{
 		if(!this.estCompteExistant(numero)) {
@@ -27,25 +28,26 @@ public class Banque extends CompteBancaire {
 			throw new ArithmeticException("solde non nul");
 		}
 		CompteBancaire c = new CompteBancaire(numero);
-		this.comptes.remove(c);
+		this.l.remove(c);
 	}
 	public boolean estCompteExistant(String numero) {
 		CompteBancaire c = new CompteBancaire(numero);
-		if(this.comptes.contains(c)) {
+		if(this.l.contains(c)) {
 			return true;
 		}else {
 			return false;
 		}
 	}
-	public CompteBancaire compte(String numero) throws ArithmeticException{
+	public CompteBancaire compte(String numero) {
 		if(!this.estCompteExistant(numero)) {
-			throw new ArithmeticException("compte non existant");
-		}
+			return null;
+		} else {
 		return new CompteBancaire(numero);
+		}	
 	}
 	@Override
 	public String toString() {
-		return "[Banque	; " + this.libelle() + " " + this.comptes + "]";
+		return "[Banque	; " + this.getLibelle() + " " + this.l + "]";
 	}
 	
 }
