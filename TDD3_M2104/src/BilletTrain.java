7472006a73fcb9bcca10111eba081e0e33101aad
hichem <krh3528a@r-info-rubi-e06.iut.iut-tlse3.fr>
2021-06-02 11:11:48 +0200

    .

--- TDD3_M2104/src/BilletTrain.java
+++ TDD3_M2104/src/BilletTrain.java
@@ -1,39 +1,44 @@
 
 public class BilletTrain {
 	private float distance;
-	public static final float[] DISTANCE= {2F, 25F, 50F, 100F, 150F, 300F};
 	public static final int[] BAREMEBORD= {10,  15,  25,   35,   60,   90};
 	public static final int[] BAREMECONTROLE= {50,  50,  50,   50,   90,  120};
+	public static final float[] DISTANCE= {2F, 25F, 50F, 100F, 150F, 300F, Float.MAX_VALUE};
 	public BilletTrain(float distance) throws IllegalArgumentException{
 		if(distance <= DISTANCE[0]) {
 			throw new IllegalArgumentException("Distance infèrieure à 2km");
 		}
 		this.distance=distance;
 	}
-	public int getTarif(boolean b) {
-		if(b == true) {
-			if(this.distance<=DISTANCE[1])
-			return BAREMEBORD[0];
-			if(this.distance<=DISTANCE[2])
-			return BAREMEBORD[1];
-			if(this.distance<=DISTANCE[3])
-			return BAREMEBORD[2];
-			if(this.distance<=DISTANCE[4])
-			return BAREMEBORD[3];
-			if(this.distance<=DISTANCE[5])
-			return BAREMEBORD[4];
-			if(this.distance>DISTANCE[5])
-			return BAREMEBORD[5];
+	public int getTarif(boolean barêmeBord) {
+		if(barêmeBord) {
+			return getTarifAvecTable(BAREMEBORD);
+		} else {
+            return getTarifAvecTable(BAREMECONTROLE);
 		}
-		if(b == false) {
-			if(this.distance<=DISTANCE[4])
-			return BAREMECONTROLE[0];
-			if(this.distance<=DISTANCE[5])
-				return BAREMECONTROLE[4];
-			if(this.distance>DISTANCE[5])
-				return BAREMECONTROLE[5];
-		}
-		return 0;
 	}
-
+	private int getTarifAvecTable(int[] tableTarif) {
+	/*if(this.distance > DISTANCE[5]) {
+		return tableTarif[5];
+	}
+	if(this.distance > DISTANCE[4]) {
+		return tableTarif[4];
+	}
+	if(this.distance > DISTANCE[3]) {
+		return tableTarif[3];
+	}
+	if(this.distance > DISTANCE[2]) {
+		return tableTarif[2];
+	}
+	if(this.distance > DISTANCE[1]) {
+		return tableTarif[1];
+	}
+	return tableTarif[0];*/
+	int i=0;
+	while(this.distance> DISTANCE[i]) {
+		i++;
+	}
+	return tableTarif[i-1];
+  }
+		
 }
